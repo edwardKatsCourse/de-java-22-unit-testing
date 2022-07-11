4bed0c2de5075faa80cd39045603f1a730da2290
unknown <edward.kats.course@gmail.com>
2022-07-11 13:37:57 +0300

    first message

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import lombok.Builder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+    // Design Patterns
+    // GoF - Gang of Four
+
+    // Creational
+    // - singleton
+    // - builder
+    // - factory method
+
+    // Structural
+    // Behavioral
+    // - iterator
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
+
+
+
+
+
+
+
